d9ab1dbdcae34371d3bdf0dec7d0d7d9ed6e3945
Nick Adair <nick.adair@live.com>
2022-12-30 12:57:14 -0600

    More Hex Container updates

--- src/components/HexContainer.vue
+++ src/components/HexContainer.vue
@@ -2,62 +2,55 @@
     <div class="hex-container">
         <div class="hex">
             <slot name="image"></slot>
-            <h2><slot name="text"></slot></h2>
-        </div> 
-    </div>     
+            <div class="hex-inner">
+                <slot name="text"></slot>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
-    @media (max-width: 767px) {        
-        .hex {
-            background-color: white;
-            clip-path: polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%);
-            line-height: 0;
-            margin: 0 auto;
-            padding: 0;
-            filter: grayscale(100%);
-            transition: filter .75s ease;
-            /* max-width: 12rem;
-            max-height: 12rem; */
-        }  
-    }
-    
+@media (max-width: 767px) {
     .hex {
-        background-color: white;
-        clip-path: polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%);
-        line-height: 0;
-        margin: 0;
-        padding: 0;
-        filter: grayscale(100%);
-        transition: filter .75s ease;
+        margin: 0 auto;
     }
+}
 
-    .hex img {
-        margin: 0;
-        padding: 0;
-    }    
+.hex {
+    background-color: white;
+    clip-path: polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%);
+    line-height: 0;
+    margin: 0;
+    padding: 0;
+    filter: grayscale(100%);
+    transition: filter .75s ease;
+}
 
-    .hex h2 {
-        color: rgba(0, 0, 0, 0);
-        text-shadow: 3px 3px 3px rgb(255, 255, 255);
-        text-transform: uppercase;
-        font-weight: bold;
-        position: absolute;
-        top: 50%;
-        left:  50%;
-        -webkit-transform: translate(-50%, -50%); /* iOS */
-        transform: translate(-50%, -50%);
-        transition-property: color;
-        transition-duration: .75s;
-        transition: color .75s ease, text-shadow .75s ease;
-    }
+.hex img {
+    margin: 0;
+    padding: 0;
+}
 
-    .hex:hover {
-        filter: none;
-    }
+.hex .hex-inner {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    -webkit-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+}
 
-    .hex:hover h2 {
-        color: rgb(255, 255, 255, 1.0);
-        text-shadow: 0 0 1px black, 0 0 1px black, 0 0 1px black, 0 0 1px black; 
-    }
+.hex:hover {
+    filter: none;
+}
+
+.hex .hex-inner a {
+    font-size: 1.5rem;
+    text-transform: uppercase;
+    text-decoration: none;
+    color: white;
+}
+
+.hex .hex-inner a:hover {
+    text-decoration: underline;
+}
 </style>
